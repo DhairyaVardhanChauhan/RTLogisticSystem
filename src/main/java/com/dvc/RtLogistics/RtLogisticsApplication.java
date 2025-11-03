e9972557e9bbf605730506c120c56fe3064ea865
salescode <salescode@salescodes-MacBook-Air.local>
2025-11-04 00:43:09 +0530

    Dev phase1-> login and signup flow

---
+++ src/main/java/com/dvc/RtLogistics/RtLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.dvc.RtLogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtLogisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dvc/RtLogistics/RtLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvc.RtLogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtLogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
